b8368f40549f491ab49017bab7cf131f1a86aa2b
singh2381 <45309644+singh2381@users.noreply.github.com>
2018-11-24 17:13:51 +0530

    second commit

--- wp-login.php
+++ wp-login.php
@@ -9,19 +9,19 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; Singh &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' href='http://singh2381.github.io/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
+<link rel='stylesheet' href='https://singh2381.github.io/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
 <meta name='robots' content='noindex,follow' />
 	<meta name="viewport" content="width=device-width" />
-	<link rel="icon" href="http://singh2381.github.io/wp-content/uploads/2018/11/Pranay-Tours-Travels-100x100.png" sizes="32x32" />
-<link rel="icon" href="http://singh2381.github.io/wp-content/uploads/2018/11/Pranay-Tours-Travels-300x300.png" sizes="192x192" />
-<link rel="apple-touch-icon-precomposed" href="http://singh2381.github.io/wp-content/uploads/2018/11/Pranay-Tours-Travels-300x300.png" />
-<meta name="msapplication-TileImage" content="http://singh2381.github.io/wp-content/uploads/2018/11/Pranay-Tours-Travels-300x300.png" />
+	<link rel="icon" href="https://singh2381.github.io/wp-content/uploads/2018/11/Pranay-Tours-Travels-100x100.png" sizes="32x32" />
+<link rel="icon" href="https://singh2381.github.io/wp-content/uploads/2018/11/Pranay-Tours-Travels-300x300.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="https://singh2381.github.io/wp-content/uploads/2018/11/Pranay-Tours-Travels-300x300.png" />
+<meta name="msapplication-TileImage" content="https://singh2381.github.io/wp-content/uploads/2018/11/Pranay-Tours-Travels-300x300.png" />
 	</head>
 	<body class="login login-action-login wp-core-ui  locale-en-us">
 		<div id="login">
 		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
 	
-<form name="loginform" id="loginform" action="http://singh2381.github.io/wp-login.php" method="post">
+<form name="loginform" id="loginform" action="https://singh2381.github.io/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email Address<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
@@ -33,13 +33,13 @@
 		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-		<input type="hidden" name="redirect_to" value="http://singh2381.github.io/wp-admin/" />
+		<input type="hidden" name="redirect_to" value="https://singh2381.github.io/wp-admin/" />
 		<input type="hidden" name="testcookie" value="1" />
 	</p>
 </form>
 
 <p id="nav">
-	<a href="http://singh2381.github.io/wp-login.php?action=lostpassword">Lost your password?</a>
+	<a href="https://singh2381.github.io/wp-login.php?action=lostpassword">Lost your password?</a>
 </p>
 
 <script type="text/javascript">
@@ -56,7 +56,7 @@ wp_attempt_focus();
 if(typeof wpOnload=='function')wpOnload();
 </script>
 
-	<p id="backtoblog"><a href="http://singh2381.github.io/">&larr; Back to Singh</a></p>
+	<p id="backtoblog"><a href="https://singh2381.github.io/">&larr; Back to Singh</a></p>
 		
 	</div>
 
